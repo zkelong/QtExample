54ce404b6e4a80dc77d6a86021eb7e0fb73d594e
zhangxiaolong <523666208@qq.com>
2016-08-19 17:52:38 +0800

    qml use c

--- CAndQML/QMLUSEC/colormaker.h
+++ CAndQML/QMLUSEC/colormaker.h
@@ -6,10 +6,29 @@
 class ColorMaker : public QObject
 {
     Q_OBJECT
+    Q_ENUMS(GenerateAlgorithm)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+    Q_PROPERTY(QColor timeColor READ timeColor)
 
 public:
     ColorMaker(QObject *parent = 0);
     ~ColorMaker();
+
+    enum GenerateAlgorithm{
+        RandomRGB,
+        RandomRed,
+        RandomGreen,
+        RandomBlue,
+        LinearIncrease
+    };
+
+    QColor color() const;
+    void setColor(const QColor & color);
+    QColor timeColor() const;
+
+    Q_INVOKABLE GenerateAlgorithm algorithm() const;
+    Q_INVOKABLE void setAlgorithm(GenerateAlgorithm algorithm);
+    //在定义一个类的成员函数时使用 Q_INVOKABLE 宏来修饰，就可以让该方法被元对象系统调用。这个宏必须放在返回类型前面。
 
 signals:
     void colorChanged(const QColor & color);
@@ -18,6 +37,14 @@ signals:
 public slots:
     void start();
     void stop();
+
+protected:
+    void timerEvent(QTimerEvent *e);
+
+private:
+    GenerateAlgorithm m_algorithm;
+    QColor m_currentColor;
+    int m_nColorTimer;
 };
 
 #endif // COLORMAKER_H
